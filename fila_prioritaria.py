3025e311a40e4d79a8230238ab8a484c3f45ed66
Gustavo Silva <gusta@DESKTOP-DUAV7CL.localdomain>
2022-09-21 02:48:41 -0300

    factory e tipagens

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,3 +1,5 @@
+from typing import Dict, Union, List
+
 from constantes import CODIGO_PRIORITARIO
 from fila_base import FilaBase
 
@@ -13,8 +15,10 @@ class FilaPrioritaria(FilaBase):
         return f'Cliente atual:  {cliente_atual}, dirija-se ao caixa: {caixa}'
 
     def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
+        estatistica: Dict[str, Union[List[str], str, int]] = {}
+
         if flag != 'detail':
-            estatistica = {f'{agencia}-{dia}': len(self.clientes_atendidos)}
+            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
         else:
             estatistica = {
                 'dia': dia,
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
 #
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
+# fila_teste = FilaNormal()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+#
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+#
+# print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+# print(fila_teste_2.estatistica('10/01/1993', 198, ''))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
-print(fila_teste_2.estatistica('10/01/1993', 198, ''))
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
